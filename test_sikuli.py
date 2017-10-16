6d4261a8492d2bfdf3974bd31252510ba754e739
zhanglijuanand <juan_up@163.com>
2017-10-16 10:52:41 +0800

    test

---
+++ test_sikuli.py
@@ -0,0 +1,9 @@
+#coding=utf-8
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+import os
+
+command ='C:\\Users\\yy\\Desktop\\sikuli\\runsikulix.cmd -r G:\\testWin\\sikuliScript\\mstscclick.sikuli'
+os.system(command)
